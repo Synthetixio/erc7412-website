e22f3d438ec1e62688063e838acd82c017ff3b66
Noah Litvin <335975+noahlitvin@users.noreply.github.com>
2024-01-16 22:32:21 -0500

    build: use pnpm

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,15 +1,16 @@
 import type { MetadataRoute } from 'next';
 
 const manifest = (): MetadataRoute.Manifest => ({
-  short_name: 'nextarter-chakra',
-  name: 'Next.js App Template',
+  short_name: 'ERC-7412',
+  name: 'ERC-7412',
   lang: 'en',
   start_url: '/',
-  background_color: '#FFFFFF',
-  theme_color: '#FFFFFF',
+  background_color: '#000000',
+  theme_color: '#000000',
   dir: 'ltr',
   display: 'standalone',
   prefer_related_applications: false,
+  /*
   icons: [
     {
       src: '/vercel.svg',
@@ -17,6 +18,7 @@ const manifest = (): MetadataRoute.Manifest => ({
       sizes: '48x48 72x72 96x96 128x128 256x256',
     },
   ],
+  */
 });
 
 export default manifest;
